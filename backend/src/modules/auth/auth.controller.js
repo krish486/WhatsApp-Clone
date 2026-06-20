c085a83362c64a85a08d3ee95428f9726cb749b0
Krish <khambhatikrish@gmail.com>
2026-06-20 19:30:13 +0530

    create google callback controller

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,11 +1,29 @@
+const AuthService = require("./auth.service")
 
 
 class AuthController {
     constructor() {
-
+        this.authService = new AuthService();
     }
     async googleCallbackController(req, res) {
-
+        const { accToken, refToken } = await this.authService.createUser(req.user)
+        accessCookieConfig = {
+            httpOnly: false,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 60 * 60 * 1000,
+        }
+        refreshCookieConfig = {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 24 * 60 * 60 * 1000,
+        }
+        res.cookie("access-token", accToken, accessCookieConfig);
+        res.cookie("refresh-token", refToken, refreshCookieConfig);
+        return res.status(200).json({
+            success: true
+        })
     }
 
 }
