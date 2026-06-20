9e651620b7c6fb5c78340f8976c500896c4f9c19
Krish <khambhatikrish@gmail.com>
2026-06-20 11:36:40 +0530

    create folder structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 let express = require("express")
 const requestLogger = require("./middleware/requestLogger")
+const authRoutes = require("./modules/auth/auth.routes")
 
 
 const createApp = () => {
     let app = express()
 
     app.use(requestLogger())
+
+    app.use("/auth", authRoutes)
 
 
     return app
---
+++ backend/src/modules/auth/auth.routes.js
@@ -0,0 +1,7 @@
+const { Router } = require("express")
+
+const authRoutes = Router()
+
+
+
+module.exports = authRoutes
