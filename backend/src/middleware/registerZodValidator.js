e9d1ceaf1d6cffb74478e77ea9414bcc2cd7d171
Krish <khambhatikrish@gmail.com>
2026-06-20 10:44:19 +0530

    create register validator middleware

---
+++ backend/src/middleware/registerZodValidator.js
@@ -0,0 +1,17 @@
+const registerValidatorMiddleware = () => {
+    return (req, res, next) => {
+        try {
+            req.body = schema.parse(req.body);
+            next();
+        }
+        catch (err) {
+            return res.status(400).json({
+                success: false,
+                errors: err.errors
+            })
+        }
+    }
+}
+
+
+module.exports = { registerValidatorMiddleware }
