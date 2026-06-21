65b30749ecd5ce4406f3e495f7face0b75aeabcc
Krish <khambhatikrish@gmail.com>
2026-06-21 23:11:11 +0530

    api-intercepters added

--- frontend/src/config/axios-Instance/axiosInstance.js
+++ frontend/src/config/axios-Instance/axiosInstance.js
@@ -4,3 +4,33 @@ export const apiInstance = axios.create({
     baseURL: "http://localhost:3000" || "https://whatsapp-clone-ipnq.onrender.com",
     withCredentials: true
 })
+
+
+
+apiInstance.interceptors.response.use((response) => response,
+    async (error) => {
+        const originalReq = error.config;
+        if (
+            error.response?.status === 401 &&
+            !originalReq._retry
+        ) {
+            originalReq._retry = true;
+            try {
+
+                await api.post("/auth/refreshToken");
+
+                return api(originalRequest);
+
+            }
+            catch (err) {
+
+                console.log("Refresh Token Expired");
+
+                window.location.href = "/login";
+
+                return Promise.reject(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+)
