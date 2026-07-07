52d8378999d3699a682fdde546aa51984c2b5afb
Krish <khambhatikrish@gmail.com>
2026-07-07 11:43:57 +0530

    verifying the bug

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client"
 import { parentApi } from "../config/parent-api/parentApi"
 
 export const socket = io("https://whatsapp-clone-ipnq.onrender.com", {
+    // export const socket = io("http://localhost:3000", {
     withCredentials: true,
     autoConnect: false,
 })
